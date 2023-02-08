32135affdedc79a663c4b9686310ea0b632b6f76
kwasniew <kwasniewski.mateusz@gmail.com>
2023-02-08 13:37:09 +0100

    update

--- .github/update-package-version.sh
+++ .github/update-package-version.sh
@@ -3,10 +3,10 @@
 yarn upgrade $1 --latest
 if [ -n "$(git status --porcelain)" ]; then
   echo "Changes detected"
-  #  yarn test
-  #  git add .
-  #  git commit -m "chore: auto deps upgrade"
-  #  git push origin $2
+  yarn test
+  git add .
+  git commit -m "chore: auto deps upgrade"
+  git push origin $2
 else
   echo "No changes detected"
 fi
